3cdf2e3c18fb068190364db83696fa736db2a44f
Hussein <89837531+Xodie256@users.noreply.github.com>
2022-10-26 12:59:46 +0300

    Added are the controllers for students

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        if(Auth::user()->hasRole('admin')){
+            return view('administrators.dashboard');
+        }
+        if(Auth::user()->hasRole('student')){
+            return view('students.dashboard');
+        }
+        if(Auth::user()->hasRole('lecturer')){
+            return view('lects.dashboard');
+        }
+
+    }
+}
